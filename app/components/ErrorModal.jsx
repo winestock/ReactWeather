2721358550082cb8b60c3375b8f58e228a13e1cb
Rudolf Winestock <rudolf@winestockwebdesign.com>
2017-06-12 20:26:18 -0400

    Update error dialog and weather page

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -7,11 +7,11 @@ var ErrorModal = React.createClass({
   },
   render: function () {
     return (
-      <div id="error-modal" className="reveal tiny text-center" data-reveal>
+      <div id="error-modal" className="reveal tiny text-center" data-reveal="">
         <h4>Some Title</h4>
         <p>Error Message</p>
         <p>
-          <button className="button hollow" data-close>
+          <button className="button hollow" data-close="">
             Okay
           </button>
         </p>
