5892c0388a8fc9d5bd40a573c6750f28a7b477bc
Rudolf Winestock <rudolf@winestockwebdesign.com>
2017-06-12 20:46:08 -0400

    Update error modal

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -23,8 +23,8 @@ var Weather = React.createClass({
         location: location,
         temp: temp,
         isLoading: false
-      })
-    }, function (errorMessage) {
+      });
+    }, function (e) {
       that.setState({
         isLoading: false,
         errorMessage: e.message
@@ -45,7 +45,7 @@ var Weather = React.createClass({
     function renderError () {
       if (typeof errorMessage === 'string') {
         return (
-          <ErrorModal/>
+          <ErrorModal message={errorMessage}/>
         )
       }
     }
